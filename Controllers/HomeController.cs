ebc82ac71634ef418acaf3e35a31eaa1b86dde53
James Leu <leujase@gmail.com>
2020-06-11 16:24:36 -0500

    initial upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using FormSubmission.Models;
+
+namespace FormSubmission.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet("")]
+        public IActionResult Form()
+        {
+            return View();
+        }
+
+        [HttpPost("success")]
+        public IActionResult Create(User user)
+        {
+          if(ModelState.IsValid)
+          {
+            ViewBag.User = user;
+            return View("Display");
+          }
+          else{
+            return View("Form");
+          }
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
